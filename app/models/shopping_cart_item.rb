d42577b0a5fec5fc5236703d0f9a2f59a4778991
Damian Szalbierz <szalbierz.d.k@gmail.com>
2025-02-24 16:07:07 +0100

    Enhance shopping cart item scope to filter by latest diet set plan creation date

--- app/models/shopping_cart_item.rb
+++ app/models/shopping_cart_item.rb
@@ -12,12 +12,18 @@ class ShoppingCartItem < ApplicationRecord
   scope :with_current_or_future_diet_set_plan, -> {
     where(<<~SQL, Date.current)
       EXISTS (
-        SELECT 1 FROM diet_set_plans mp
+        SELECT 1
+        FROM diet_set_plans mp
         JOIN diet_sets ds ON mp.diet_set_id = ds.id
         JOIN meals m ON m.diet_set_id = ds.id
         JOIN products p ON p.meal_id = m.id
         WHERE p.id = shopping_cart_items.product_id
           AND mp.date >= ?
+          AND mp.created_at = (
+            SELECT MAX(mp2.created_at)
+            FROM diet_set_plans mp2
+            WHERE mp2.date = mp.date
+          )
       )
     SQL
   }
